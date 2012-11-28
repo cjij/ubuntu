c80f9bdef9d7ac0466126aff6dc3e142923e1e98
Vladimir Valyukh <vlava115@student.liu.se>
2012-11-28 02:35:19 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #/bin/bash
 
 sudo apt-get update
-sudo apt-get dist-upgrade
+sudo apt-get dist-upgrade -y
+
+sudo apt-get install -y openssh-server apache2 php5 git
+
+sudo update-rc.d apache2 disable
+sudo touch /etc/ssh/sshd_not_to_be_run
