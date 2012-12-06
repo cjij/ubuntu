44414786cadd88832213b731ed8182807d18b26b
vlad <vlad@nx.(none)>
2012-12-06 01:25:14 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 
-sudo apt-get install -y openssh-server apache2 php5 git gnome-shell
+sudo apt-get install -y openssh-server apache2 php5 git gnome-shell vim vim-common vim-gtk git
 
 sudo update-rc.d apache2 disable
 sudo touch /etc/ssh/sshd_not_to_be_run
+
+cd
+git clone https://github.com/skwp/dotfiles ~/.yadr
+cd ~/.yadr && rake install
+cd
