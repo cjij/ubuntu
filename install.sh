4dc2ba2f22ff6b1f5b65a489f9122d9713a1ad44
Vladimir Valyukh <vladimir.sensei@gmail.com>
2012-12-21 06:24:04 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 
-sudo apt-get install -y openssh-server apache2 php5 git gnome-shell vim vim-common vim-gtk git bison flex wine vlc terminator
+sudo apt-get install -y openssh-server apache2 php5 git gnome-shell vim vim-common vim-gtk git bison flex wine vlc terminator subdownloader \
+  acidrip ubuntu-restricted-extras
+
+sudo /usr/share/doc/libdvdread4/install-css.sh
 
 sudo update-rc.d apache2 disable
 sudo touch /etc/ssh/sshd_not_to_be_run
