b5c43095a560ed524acc2a3f474d354e3a9b8fae
vlad-sensei <vladimir.sensei@gmail.com>
2013-05-04 18:51:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get dist-upgrade -y
 sudo apt-get install -y openssh-server apache2 php5 git gnome-shell \
   vim vim-common vim-gtk git bison flex wine vlc terminator subdownloader \
   acidrip ubuntu-restricted-extras codeblocks rake default-jre g++ virtualbox \
-  nmap wireshark aircrack-ng ettercap* macchanger chromium-browser privoxy
+  nmap wireshark aircrack-ng ettercap-graphical macchanger chromium-browser privoxy
 
 sudo /usr/share/doc/libdvdread4/install-css.sh
 
