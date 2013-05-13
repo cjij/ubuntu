26705d598168fafca84871354a152d88faf45d9e
Vladimir Valyukh <vladimir.sensei@gmail.com>
2013-05-14 01:01:45 +0200

    modified:   chmac.sh

--- chmac.sh
+++ chmac.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# sudo cp chmac.sh /etc/init.d/chmac
+# sudo update-rc.d chmac defaults 10
 
-for iface in eth0, wlan0
+for iface in eth0 wlan0
 do
   ifconfig $iface down
   /usr/bin/macchanger -A $iface
