61ba52edb3a4f89a7090da970a8d655207ed532a
Vladimir Valyukh <vladimir.sensei@gmail.com>
2013-06-10 12:13:28 +0200

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y openssh-server apache2 php5 git gnome-shell \
   vim vim-common vim-gtk git bison flex wine vlc terminator subdownloader \
   acidrip ubuntu-restricted-extras codeblocks rake default-jre g++ virtualbox \
   nmap wireshark aircrack-ng ettercap-graphical macchanger chromium-browser \
-  privoxy zsh
+  privoxy zsh build-essential linux-headers-generic
 
 sudo /usr/share/doc/libdvdread4/install-css.sh
 
